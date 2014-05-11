7a2e5c3de5b55d831456a0bca215a9595bffc1e3
khal <khal.rahman@gmail.com>
2014-05-11 10:22:44 +0800

    eiguide scaffolding commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Eiguide
+
+A guide tour framework for Einsights
+
+## Getting Started
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/krahman/eiguide/master/dist/eiguide.min.js
+[max]: https://raw.github.com/krahman/eiguide/master/dist/eiguide.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/eiguide.min.js"></script>
+<script>
+jQuery(function($) {
+  $.eiguide(); // "eiguide"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
---
+++ dist/eiguide.js
@@ -0,0 +1,33 @@
+/*! Eiguide - v0.1.0 - 2014-05-11
+* https://github.com/krahman/eiguide
+* Copyright (c) 2014 khal; Licensed MIT */
+(function($) {
+
+  // Collection method.
+  $.fn.eiguide = function() {
+    return this.each(function(i) {
+      // Do something awesome to each selected element.
+      $(this).html('awesome' + i);
+    });
+  };
+
+  // Static method.
+  $.eiguide = function(options) {
+    // Override default options with passed-in options.
+    options = $.extend({}, $.eiguide.options, options);
+    // Return something awesome.
+    return 'awesome' + options.punctuation;
+  };
+
+  // Static method default options.
+  $.eiguide.options = {
+    punctuation: '.'
+  };
+
+  // Custom selector.
+  $.expr[':'].eiguide = function(elem) {
+    // Is this element awesome?
+    return $(elem).text().indexOf('awesome') !== -1;
+  };
+
+}(jQuery));
